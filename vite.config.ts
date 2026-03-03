d0f81e69f5d2728a486e8fbd549c86abf5b0257e
Kangfu00 <121802961+Kangfu00@users.noreply.github.com>
2026-03-03 18:20:54 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'url';
+
+// สร้าง __dirname และ __filename สำหรับ ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: './',
+      base: './', 
       server: {
         port: 3000,
         host: '0.0.0.0',
